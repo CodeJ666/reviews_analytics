b7282c89bd1844f9257311d8a220f1c89b95aa31
Josh Su <josheating1206@gmail.com>
2021-03-09 21:26:36 +0800

    calc. avg length

--- read.py
+++ read.py
@@ -4,5 +4,9 @@ with open('reviews.txt', 'r') as f:
         data.append(line.strip())
         if len(data) % 1000 == 0:
             print(len(data))
-print(len(data))
-print(data[0])
+print('檔案讀取完了, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言平均長度是', sum_len / len(data), '個字母')
