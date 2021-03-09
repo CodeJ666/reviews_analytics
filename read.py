ab1320225031e43a294c68d3800dbe4bfd466e83
Josh Su <josheating1206@gmail.com>
2021-03-09 21:39:28 +0800

    final version:

--- read.py
+++ read.py
@@ -2,7 +2,7 @@ data = []
 with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line.strip())
-        if len(data) % 1000 == 0:
+        if len(data) % 10000 == 0:
             print(len(data))
 print('檔案讀取完了, 總共有', len(data), '筆資料')
 
@@ -10,3 +10,19 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('留言平均長度是', sum_len / len(data), '個字母')
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
+
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言說這很讚')
+print(good[0])
